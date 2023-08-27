3d756095930e277793155b82df6c6b614c43dda0
João Souza <jvcsouza12@gmail.com>
2023-08-27 02:16:51 -0300

    .

--- src/components/section/index.jsx
+++ src/components/section/index.jsx
@@ -1,10 +1,10 @@
 export default function Section({children, title, icon: Icon}){
     return(
         <section 
-            className = 'relative h-[100vh] w-full px-4'>
+            className = 'relative h-[100vh] w-full flex justify-center'>
             { title && 
                 <div className = 'flex gap-2 items-center'>
-                    <h1 className = 'text-4xl text-center text-primary font-bold w-full'>{title}</h1>
+                    <h1 className = 'text-4xl text-primary font-bold'>{title}</h1>
                     {Icon && 
                         <div>
                             <Icon className = 'text-3xl text-primary'/>
@@ -12,7 +12,7 @@ export default function Section({children, title, icon: Icon}){
                 </div>
             }
             
-            {children}
+                {children}
             
         </section>
     )
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -6,55 +6,55 @@ import {MdDeveloperMode, MdLocationOn} from 'react-icons/md';
 
 export default function AboutMe(){
     return(
-        <Section title = 'Sobre mim'>
-            {/* <div className = 'flex justify-center  p-4 rounded-2xl w-full'>
-                <div className = ' w-[50%]'>
-                    <div className = 'flex items-center gap-2'>
+        <Section>
+            <div className = 'flex flex-col justify-center rounded-2xl w-full h-full'>
+                <div className = ' w-full h-1/2'>
+                    <div className = 'flex flex-col gap-8 justify-center items-start gap-2'>
 
-                        <div>
+                        <div className = 'flex gap-2'>
                             <img
-                                className = 'w-56 h-56 object-cover rounded-full shadow-2xl shadow-primary'
+                                className = 'w-20 h-20 object-cover rounded-full'
                                 src= {fotosocial} 
                                 alt="" 
                             />
-                        </div>
 
-                        <div className = 'flex flex-col gap-7'>
                             <div className = 'flex flex-col gap-1 '>
-                                <label className = 'text-primary text-2xl font-bold '> João Vitor da Cunha Souza</label>
+                                <label className = 'text-primary text-lg font-bold '> João Vitor da Cunha Souza</label>
                                 <div className = 'flex items-center gap-2 '>
                                     <MdDeveloperMode />
-                                    <label className = 'text-lg text-gray-dark'>Web Developer</label>
+                                    <label className = 'text-md text-center text-gray-dark'>Web Developer</label>
                                 </div>
                             </div>
-
-                            <div className = 'flex flex-col gap-3'>
-                                <label className = 'flex gap-1 items-center'>
-                                    <HiAcademicCap className = 'text-xl'/>
-                                    <div className = 'relative'>
-                                        <div>Bacharelado em Sistemas de informação</div>
-                                        <div className = 'absolute top-[-1.5rem] right-[-1rem] p-1 text-[10px] bg-light font-bold text-dark rounded-md' >
-                                            Cursando...
-                                        </div>
-                                    </div>
-                                </label>
-
-                                <label className = 'flex gap-1 items-center'>
-                                    <FaBirthdayCake className = 'text-xl'/>
-                                    31/10/2002 (20 anos)
-                                </label>
-
-                                <label className = 'flex gap-1 items-center'>
-                                    <MdLocationOn className = 'text-xl'/>
-                                    PortoVelho (RO) | Brasil
-                                </label>
-                            </div>
                         </div>
+
+            
+                        <div className = 'flex flex-col items-start w-full gap-3'>
+                            <label className = 'flex gap-1'>
+                                <HiAcademicCap className = 'text-xl'/>
+                                <div className = 'relative'>
+                                    <div className = 'text-sm'>Bacharelado em Sistemas de informação</div>
+                                    <div className = 'absolute top-[-1.5rem] right-[-1rem] p-1 text-[10px] bg-light font-bold text-dark rounded-md' >
+                                        Cursando...
+                                    </div>
+                                </div>
+                            </label>
+
+                            <label className = 'flex gap-1 items-center'>
+                                <FaBirthdayCake className = 'text-xl'/>
+                                <div className='text-sm'>31/10/2002 (20 anos)</div>
+                            </label>
+
+                            <label className = 'flex gap-1 items-center'>
+                                <MdLocationOn className = 'text-xl'/>
+                                <div className='text-sm'>PortoVelho (RO) | Brasil</div>
+                            </label>
+                        </div>
+                        
                     </div>
                 </div>
 
-                <div className = 'p-6 w-1/2 bg-backgroundhover rounded-md'>
-                    <div className = 'flex flex-col justify-around h-full gap-2'>
+                <div className = 'p-6 w-full bg-backgroundhover rounded-md h-full mt-8'>
+                    <div className = 'flex flex-col items-center justify-around h-full gap-2 overflow-y-auto'>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident aut debitis,
                             sit nemo odio dolorum numquam dignissimos ab dicta
                             veniam, nihil iure facere natus! Optio, officiis? Numquam eius a asperiores.
@@ -65,7 +65,7 @@ export default function AboutMe(){
                         </p>
                     </div>
                 </div>
-            </div> */}
+            </div>
         </Section>
     )
 }
