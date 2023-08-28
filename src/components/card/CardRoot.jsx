675a887494d85e67c781064fb9c7eba5dc8133fd
João Souza <jvcsouza12@gmail.com>
2023-08-28 01:05:42 -0300

    .

--- src/components/card/CardRoot.jsx
+++ src/components/card/CardRoot.jsx
@@ -2,7 +2,7 @@
 export function CardRoot({children}){
     return(
         
-        <div className = 'max-w-[300px] lg: max-w-[400px] h-full  flex shrink-0 grow-0 flex-col justify-around shrink-0 border-2 border-dark rounded-xl p-4 gap-[1rem]'>
+        <div className = 'w-full lg: max-w-[400px] h-full  flex shrink-0 grow-0 flex-col justify-around shrink-0 border-2 border-dark rounded-xl p-4 gap-[1rem]'>
             {children}
         </div>
         
