bd4448956f0cc57cd6bf50c196be61b465520a82
João Souza <jvcsouza12@gmail.com>
2023-08-30 14:49:57 -0300

    .

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -9,16 +9,16 @@ export default function AboutMe(){
         <Section>
             <div className = 'flex flex-col justify-center gap-8 w-full h-full md:flex-row md:items-center lg:gap-1'>
                 <div className = ' w-full'>
-                    <div className = 'flex flex-col gap-8 justify-center items-start lg:flex-row lg:items-center'> 
+                    <div className = 'flex flex-col gap-3 justify-center items-start lg:flex-row lg:items-center'> 
 
-                        <div className = 'flex gap-2  md:flex-row lg:w-56 lg:h-56 lg:items-center'>
+                        <div className = 'flex gap-3  md:flex-row lg:w-56 lg:h-56 lg:items-center'>
                             <img
                                 className = 'w-20 h-20 object-cover rounded-full lg:w-40 lg:h-40'
                                 src= {fotosocial} 
                                 alt="" 
                             />
 
-                            <div className = 'flex flex-col justify-between lg:hidden'>
+                            <div className = 'flex flex-col justify-center lg:hidden'>
                                 <label className = 'text-primary text-lg font-bold sm:text-2xl md:text-2xl'> João Vitor da Cunha Souza</label>
                                 <div className = 'flex items-center gap-2 '>
                                     <MdDeveloperMode />
@@ -40,7 +40,7 @@ export default function AboutMe(){
                             <label className = 'flex items-center gap-1'>
                                 <HiAcademicCap className = 'text-xl sm:text-2xl'/>
                                 <div className = 'relative'>
-                                    <div className = 'text-sm sm:text-xl'>Bacharelado em Sistemas de informação</div>
+                                    <div className = 'text-sm sm:text-lg'>Bacharelado em Sistemas de informação</div>
                                     <div className = 'absolute top-[-1.5rem] right-[-1rem] p-1 text-[10px] bg-light font-bold text-dark rounded-md' >
                                         Cursando...
                                     </div>
@@ -48,28 +48,31 @@ export default function AboutMe(){
                             </label>
 
                             <label className = 'flex gap-1 items-center'>
-                                <FaBirthdayCake className = 'text-xl sm:text-2xl'/>
-                                <div className='text-sm sm:text-xl'>31/10/2002 (20 anos)</div>
+                                <FaBirthdayCake className = 'text-lg sm:text-2xl'/>
+                                <div className='text-sm sm:text-lg'>31/10/2002 (20 anos)</div>
                             </label>
 
                             <label className = 'flex gap-1 items-center'>
                                 <MdLocationOn className = 'text-xl sm:text-2xl'/>
-                                <div className='text-sm sm:text-xl'>PortoVelho (RO) | Brasil</div>
+                                <div className='text-sm sm:text-lg'>PortoVelho (RO) | Brasil</div>
                             </label>
                         </div>
                         
                     </div>
                 </div>
 
-                <div className = 'p-6 w-full bg-backgroundhover rounded-md lg:w-[60%]'>
+                <div className = 'p-6 w-full bg-backgroundhover rounded-md lg:w-[100%]'>
                     <div className = 'flex flex-col items-center justify-around h-full gap-2 overflow-y-auto'>
-                        <p className = 'md:text-lg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident aut debitis,
-                            sit nemo odio dolorum numquam dignissimos ab dicta
-                            veniam, nihil iure facere natus! Optio, officiis? Numquam eius a asperiores.
+                        <p className = 'md:text-lg'>
+                            Sou um intusiasta apaixonado por programação e <strong>desenvolver soluções</strong> utilizando o ecossitema JavaScript.
                         </p>
-                        <p className = 'md:text-lg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident aut debitis,
-                            sit nemo odio dolorum numquam dignissimos ab dicta
-                            veniam, nihil iure facere natus! Optio, officiis? Numquam eius a asperiores.
+
+                        <p className = 'md:text-lg'>
+                            Utilizo <strong>React</strong> para criar e gerenciar interfaces além de <strong>TailwindCss e StyledComponets</strong> para estilizar componentes.
+                        </p>
+
+                        <p className = 'md:text-lg'>
+                            Venho me capacitando em <strong>node js</strong> visando criar aplicações cada vez mais abrangentes e sofisticadas, sempre em busca de aprendizado e experiências.
                         </p>
                     </div>
                 </div>
