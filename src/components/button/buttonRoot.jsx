ad45a2e370e28e4639394ca774bd1ba513bd3c8a
JoaoSouz4 <joaosouz8@gmail.com>
2025-03-13 00:10:47 -0300

    .

--- src/components/button/buttonRoot.jsx
+++ src/components/button/buttonRoot.jsx
@@ -6,7 +6,7 @@ export function ButtonRoot({ children, style, action }) {
     <button
       className={
         style == 'leaked'
-          ? 'bg-transparent border-2 border-primary rounded-xl px-4 py-2 hover:bg-backgroundhover h-10 items-center flex justify-center'
+          ? 'bg-transparent border-2 border-primary rounded-xl px-4 py-2 hover:bg-backgroundhover h-10 items-center flex justify-center py-6'
           : 'h-10 bg-primary rounded-md px-9 py-2 hover:duration-200 hover:ease hover:bg-[#1E8D76] flex items-center justify-center rounded-xl py-6'
       }
       onClick={handleClick}
